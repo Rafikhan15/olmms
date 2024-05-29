2fcaf18493575cfe153696034770bd8bed4e3680
sunnysakib <sakibur.rahaman033@gmail.com>
2024-05-29 13:34:45 +0600

    fix: modify user table

--- frontend/src/pages/HomePage/UserTable.jsx
+++ frontend/src/pages/HomePage/UserTable.jsx
@@ -33,17 +33,19 @@ const [userData, setUserData] = useState([]);
         <Table>
           <TableHeader>
             <TableRow >
-              <TableHead className="pl-5">Id</TableHead>
+              <TableHead className="pl-5 font-medium">ID</TableHead>
               <TableHead >Username</TableHead>
               <TableHead >Email</TableHead>
+              <TableHead >Role</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             {userData.map((item) => (
               <TableRow  key={item.id}>
-                <TableCell className="pl-5 font-semibold">{item.id}</TableCell>
+                <TableCell className="pl-5 font-medium">{item.id}</TableCell>
                 <TableCell >{item.username}</TableCell>
-                <TableCell className="font-medium">{item.email}</TableCell>
+                <TableCell >{item.email}</TableCell>
+                {item.role === "admin" ? <TableCell className="font-medium text-green-600">{item.role}</TableCell> : <TableCell >{item.role}</TableCell>}
               </TableRow>
             ))}
           </TableBody>
